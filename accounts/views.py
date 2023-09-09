384f65e55a66047ac7167763ad259788a491a8ed
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-09 11:15:50 +0600

    template inheritance done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def signin(request):
+    return render(request, 'accounts/signin.html')
+
+
+def profile(request):
+    return render(request,'accounts/dashboard.html')
