925fe64436cebc6d405a953a90852e6db8677faf
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-09 20:29:52 +0600

    stores products added to display

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
+from category.models import Category
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.filter(is_available = True)
+    total_product = products.count()
+    categories = Category.objects.all()
+    return render(request, 'store/store.html', {'products':products, 'total_product':total_product, 'categories':categories})
 
 def product_details(request):
     return render(request, 'store/product-detail.html')
