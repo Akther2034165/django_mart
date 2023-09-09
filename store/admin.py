4dc9f746abd0ff066c2342f3195f7d9660fd3792
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-09 12:39:25 +0600

    product model added and handle media files errors

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('product_name',)}
+    list_display = ('product_name', 'slug', 'price', 'category', 'stock', 'is_available', 'created_date', 'modified_date',)
+
+admin.site.register(Product,ProductAdmin)
