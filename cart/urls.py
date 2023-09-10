ac53d7089bb19a04c64ee1d068ea895659f085d6
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-10 21:22:51 +0600

    cart implemented

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
-    path('<int:product_id>/', views.add_to_cart, name='add_to_cart')
+    path('<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_from_cart'),
+    path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,27 @@ from store.models import Product
 from .models import Cart, CartItem
 # Create your views here.
 def cart(request):
-    return render(request, 'cart/cart.html')
+    session_id = request.session.session_key
+    cartid = Cart.objects.get(cart_id = session_id)
+    cart_id = Cart.objects.filter(cart_id = session_id).exists()
+    cart_items = None
+    tax = 0
+    total = 0
+    grand_total = 0
+    
+    if cart_id:
+        cart_items = CartItem.objects.filter(cart = cartid)
+        for item in cart_items:
+            total += item.product.price * item.quantity
+    tax = (2*total)/100
+    grand_total = total + tax 
+    context = {
+        'cart_items':cart_items,
+        'tax': tax,
+        'total' : total,
+        'grand_total':grand_total,
+    }
+    return render(request, 'cart/cart.html',context)
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id = product_id)
@@ -31,4 +51,27 @@ def add_to_cart(request, product_id):
         )
         cart.save()
         
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    product = Product.objects.get(id = product_id)
+    session_id = request.session.session_key
+    cartid = Cart.objects.get(cart_id = session_id)
+    
+    cart_item = CartItem.objects.get(cart = cartid, product = product)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+        
+    return redirect('cart')
+    
+def remove_cart(request,product_id):
+    product = Product.objects.get(id = product_id)
+    session_id = request.session.session_key
+    cartid = Cart.objects.get(cart_id = session_id)
+    cart_item = CartItem.objects.get(cart = cartid, product = product)
+    cart_item.delete()
+    
     return redirect('cart')
